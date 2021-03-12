97748ace1ea097fb8275f1909059c4f0093512d0
abdulnafea <66330574+alsa7err90@users.noreply.github.com>
2021-03-12 12:13:52 +0200

    Update 2021_02_19_145245_create_magroles_table.php

--- src/db/2021_02_19_145245_create_magroles_table.php
+++ src/db/2021_02_19_145245_create_magroles_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class CreateMagrolesTable extends Migration
 {
@@ -19,6 +20,24 @@ class CreateMagrolesTable extends Migration
             $table->String('slug')->unique();
             $table->timestamps();
         });
+        
+        DB::table('magroles')->insert(
+            array(
+                [
+                    'name' => 'admin',
+                    'slug' => 'admin'
+                ],
+                [
+                    'name' => 'editor',
+                    'slug' => 'editor'
+                ],
+                [
+                    'name' => 'user',
+                    'slug' => 'user'
+                ]
+
+            )
+        );
     }
 
     /**
